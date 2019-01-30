9eb983eb8dcf38325845dce6ff3f34becdb227f3
Benedikt <mail@benedikt-schwering.de>
2019-01-30 20:35:10 +0100

    joo

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from functions import derivative
 from functions import integral
 from sympy import symbols
+import os
 
 print("Willkommen bei SToftware0 by bschwering & ftuente")
 
-print("Bitte gib eine Aufgabe ein! (Bsp.: 1. Ableitung von x**2)")
-keyinput = input()
 
-derivative.hook(keyinput)
-integral.hook(keyinput)
+def main():
+    print("Bitte gib eine Aufgabe ein! (Bsp.: 1. Ableitung von x**2)")
+    keyinput = input()
+
+    derivative.hook(keyinput)
+    integral.hook(keyinput)
+    main()
+
+main()
