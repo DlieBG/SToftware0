4aa63022d90d752283815f5d822ce300dfe54925
Benedikt <mail@benedikt-schwering.de>
2019-01-31 13:43:23 +0100

    New Struckture

--- web/call/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-from SToftware0.functions import derivative
-
-def index(request):
-    return HttpResponse(derivative.hook("ableitung x**2"))
--- web/urls.py
+++ web/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('call/', include('call.urls')),
+    path('call/', include('webcall.urls')),
     #path('admin/', admin.site.urls),
 ]
