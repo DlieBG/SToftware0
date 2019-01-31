ed2010a6238f1f9b13e35651df5fb4bf6595eaa5
Benedikt <mail@benedikt-schwering.de>
2019-01-31 14:49:01 +0100

    web

--- web/settings.py
+++ web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g1wt*#$j+otj@%(gu86^slktc%)p6_)r+0ogd-!*c=6^afao3p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['benedikt-schwering.de']
+ALLOWED_HOSTS = ['benedikt-schwering.de', '127.0.0.1', '192.168.0.97']
 
 
 # Application definition
