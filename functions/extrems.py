123cb3b293a7241c996ba159daf30c41954c098a
Benedikt <mail@benedikt-schwering.de>
2019-01-31 17:31:02 +0100

    Create extrems.py

---
+++ functions/extrems.py
@@ -0,0 +1,28 @@
+from sympy import *
+
+def hook(keyinput):
+    #Keywords definieren
+    if "extremstellen" in keyinput.lower() or "extrempunkte" in keyinput.lower() or "maxima" in keyinput.lower() or "minima" in keyinput.lower() or
+        return getComponents(keyinput)
+    return ""
+
+def getComponents(keyinput):
+
+    #Variablen
+
+    parts = keyinput.split(' ')
+    for part in parts:
+        #Teile den Variablen zuordnen
+    
+    if  is not "": #Abbruchbedingung
+        return functionX()
+    else:
+        print("Fehler")
+        return "Fehler"
+        
+def functionX():
+    #Berechnung der 
+    return output(out, term)
+
+def output(out, term):
+    #Ausgabe der Werte
