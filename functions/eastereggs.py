692271a478f56fe1aa3aa32cb7c3ec2ad7f6bffc
Benedikt <mail@benedikt-schwering.de>
2019-02-01 11:50:19 +0100

    Update eastereggs.py

--- functions/eastereggs.py
+++ functions/eastereggs.py
@@ -3,16 +3,16 @@ from sympy import *
 def hook(keyinput):
     if "chicago" in keyinput.lower():
         print("Ich komm immer klar auf n Chicago!")
-        return "Ich komm immer klar auf n Chicago!"
+        return "<h3>Ich komm immer klar auf n Chicago!</h3>"
     if "hello there" in keyinput.lower():
         print("General Kenobi!")
-        return "General Kenobi!"
+        return "<h3>General Kenobi!</h3>"
     if "benedikt" in keyinput.lower():
         print("Benedikt Schwering\n")
-        return "Benedikt Schwering<br>CEO von Schwering Software und SToftware<br>Lead Programmer von SToftware0<br>Technischer Informatiker"
+        return "<h3>Benedikt Schwering<br>CEO von Schwering Software und SToftware<br>Lead Programmer von SToftware0<br>Technischer Informatiker</h3>"
     if "florian" in keyinput.lower():
         print("Florian Tünte\n")
-        return "Florian T&uuml;nte<br>CFO von SToftware<br>Programmer von SToftware0<br>Informatiker"
+        return "<h3>Florian T&uuml;nte<br>CFO von SToftware<br>Programmer von SToftware0<br>Informatiker</h3>"
     if "tarik" in keyinput.lower():
         print("Tarik Krämer\n")
         return "Tarik Kr&auml;mer<br>Designer bei SToftware"
