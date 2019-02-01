5f96b74bf32f3def841477e547600a05313620ee
Benedikt <mail@benedikt-schwering.de>
2019-02-01 11:44:11 +0100

    Update eastereggs.py

--- functions/eastereggs.py
+++ functions/eastereggs.py
@@ -7,4 +7,7 @@ def hook(keyinput):
     if "hello there" in keyinput.lower():
         print("General Kenobi!")
         return "General Kenobi!"
+    if "benedikt" in keyinput.lower():
+        print("Benedikt Schwering\n")
+        return "Benedikt Schwering\nCEO von Schwering Software und SToftware\nLead Programmer von SToftware0\nTechnischer Informatiker\n"
     return ""
