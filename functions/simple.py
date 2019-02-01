128a44966d94532e5328635638d8b843bf488aaf
Benedikt <mail@benedikt-schwering.de>
2019-02-01 10:59:33 +0100

    Update simple.py

--- functions/simple.py
+++ functions/simple.py
@@ -13,7 +13,7 @@ def getComponents(keyinput):
         
         parts = keyinput.split(' ')
         for part in parts:
-            if "x" in part or "*" in part or "+" in part or "/" in part or "-" in part:
+            if "*" in part or "+" in part or "/" in part or "-" in part:
                 term = part
                 if "=" in term:
                     term = term.split("=")[1]
@@ -33,8 +33,6 @@ def getComponents(keyinput):
         for part in parts:
             if "=" in part:
                 term = part
-                if "=" in term:
-                    term = term.split("=")[1]
                 left = part.split("=")[0]
                 right = part.split("=")[1]
     
