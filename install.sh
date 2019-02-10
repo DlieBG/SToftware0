8ea8bc0c3e6d49282940b371b75e77c3ff885b4f
boba2fett <boba2002fett@gmail.com>
2019-02-10 13:44:24 +0100

    better support for Linux users without python3-sympy package XD

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-apt install python3 python3-sympy cowsay -y
-cowsay READY
+apt install python3 -y
+apt install python3-sympy -y
+if [ $? -eq 1 ] ; then
+    echo "It looks your Linux has no python3-sympy package\n please look under dependencies an install it yourself"
+fi
