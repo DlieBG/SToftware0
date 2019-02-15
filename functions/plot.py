440a3e18238b0e35c59d698d2d0594884d88c378
Benedikt <mail@benedikt-schwering.de>
2019-02-15 14:15:38 +0100

    asd

--- functions/plot.py
+++ functions/plot.py
@@ -25,4 +25,4 @@ def getComponents(keyinput):
 
 def output(out):
     print("")
-    return "\n<iframe frameborder='0' scrolling='no' style='width: 98vw; height: 350px' src='http://benedikt-schwering.de/SToftware0/python/SToftware0/html/mobile/plot/index.html?"+out+"'></iframe><br>"
+    return "\n<iframe frameborder='0' scrolling='no' style='width: 98vw; height: 350px' src='http://benedikt-schwering.de/SToftware0/python/SToftware0/html/mobile/functions/plot/index.html?"+out+"'></iframe><br>"
