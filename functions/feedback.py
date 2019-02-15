5be4bd8f490589c1ea6b689ace49be5ccec35df0
DlieBG <mail@benedikt-schwering.de>
2019-02-15 15:12:40 +0100

    joo

--- functions/feedback.py
+++ functions/feedback.py
@@ -7,5 +7,5 @@ def hook(keyinput):
     return ""
 
 def output(keyinput):
-    print("Bewertung ist nur Online verfügbar!\nWenn Sie so krass sind, dass sie die Kommandozeilenfunktion nutzen, können Sie uns Ihre Ideen einfach auf Github mitteilen!")
+    print("Bewertung ist nur Online verfügbar!\nWenn Sie so krass sind, dass sie die Kommandozeilenfunktion nutzen, können Sie uns Ihre Ideen einfach auf GitHub mitteilen!")
     return "<meta http-equiv='refresh' content='0; URL=http://benedikt-schwering.de/SToftware0/python/SToftware0/html/mobile/functions/bewertung.php'>"
