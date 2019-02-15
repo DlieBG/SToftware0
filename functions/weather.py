c640f1f493880eacb42366ab37a6960976c42ee3
DlieBG <mail@benedikt-schwering.de>
2019-02-15 15:17:55 +0100

    Wetter funktion

---
+++ functions/weather.py
@@ -0,0 +1,27 @@
+from sympy import *
+
+def hook(keyinput):
+    if "wetter" in keyinput:
+        return getComponents(keyinput)
+    return ""
+
+def getComponents(keyinput):
+
+    ort = ""
+
+    parts = keyinput.split(' ')
+    for part in parts:
+        #Teile den Variablen zuordnen
+    
+    if  is not "": #Abbruchbedingung
+        return functionX()
+    else:
+        print("Fehler")
+        return "Fehler"
+        
+def functionX():
+    #Berechnung der 
+    return output(out, term)
+
+def output(out, term):
+    #Ausgabe der Werte
