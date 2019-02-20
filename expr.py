d3992ac0a322a3a84012e8adb76595826ac04ff0
boba2fett <boba2002fett@gmail.com>
2019-02-20 22:16:49 +0100

    idee für Term Probleme

---
+++ expr.py
@@ -0,0 +1,26 @@
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
+
+
+def isTerm(term):
+    try:
+        parse_expr(term)
+    except:
+        return False
+    finally:
+        return True
+
+def findTerm(keyinput):
+    parts = keyinput.split(' ')
+    for part in parts:
+        if isTerm(part) and not isKeyword(part):
+            return part
+            
+    return ""
+    
+def isKeyword(s):
+    keywords=["ableit","derivate"]#TODO add all options if included
+    for phrase in keywords:
+        if phrase in s:
+            return True
+    return False
