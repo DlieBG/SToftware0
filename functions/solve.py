1ab3fbf7e5a4b537915d1fd3f7e3643cef99fbe0
Benedikt Schwering <mail@benedikt-schwering.de>
2019-02-24 20:23:29 +0100

    ö to oe

--- functions/solve.py
+++ functions/solve.py
@@ -2,7 +2,7 @@ from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
 
 def hook(keyinput):
-    if ("löse" in keyinput or "lösen" in keyinput or "gleichung" in keyinput or "solve" in keyinput or "solver" in keyinput) and "=" in keyinput:
+    if ("loese" in keyinput or "gleichung" in keyinput or "solve" in keyinput or "solver" in keyinput) and "=" in keyinput:
         return getComponents(keyinput)
     return ""
 
@@ -20,13 +20,13 @@ def getComponents(keyinput):
                 term = term.split("=")[1]
             left = part.split("=")[0]
             right = part.split("=")[1]
-    
+
     if term is not "" and left is not "" and right is not "":
         return solves(term, left, right)
     else:
         print("Fehler")
         return "Fehler"
-        
+
 def solves(term, left, right):
     x = symbols('x')
     init_printing(use_unicode=True)
