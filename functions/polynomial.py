ad9084cc348ae413d19ebc51f9b076377b4a3e16
Benedikt Schwering <mail@benedikt-schwering.de>
2019-03-01 17:49:42 +0100

    Rechtschreibfehler behoben

--- functions/polynomial.py
+++ functions/polynomial.py
@@ -15,13 +15,13 @@ def getComponents(keyinput):
         if "/" in part:#TODO optimize for / in a or b  Maybe tomorrow XD
             a = part.split("/")[0]
             b = part.split("/")[1]
-    
+
     if a is not "" and b is not "":
         return polinomdiv(a, b)
     else:
         print("Fehler")
         return "Fehler"
-        
+
 def polinomdiv(a, b):
     x = symbols('x')
     init_printing(use_unicode=True)
@@ -29,5 +29,5 @@ def polinomdiv(a, b):
     return output(out, a, b)
 
 def output(out, a, b):
-    print("\nPloinomdivision von "+str(a)+"/"+str(b)+" ist: \n"+str(out))
+    print("\nPolinomdivision von "+str(a)+"/"+str(b)+" ist: \n"+str(out))
     return "\nPolinomdivision: $$"+str(latex(out))+"$$"
