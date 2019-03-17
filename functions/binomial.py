df4713ea093e05c378f01ccec3246fd3b9d2a741
Benedikt <mail@benedikt-schwering.de>
2019-03-17 21:38:52 +0100

    yxc

--- functions/binomial.py
+++ functions/binomial.py
@@ -9,28 +9,28 @@ def hook(keyinput):
 
 def getComponents(keyinput):
 
-    n = ""
-    k = ""
+    n = -1
+    k = -1
 
     parts = keyinput.split(' ')
     for part in parts:
         if part.isdigit():
-            if n is "":
+            if n is -1:
                 n = int(part)
             else:
                 k = int(part)
 
 
-    if k is not "" and n is not "": #Abbruchbedingung
+    if k is not -1 and n is not -1: #Abbruchbedingung
         return nT(n, k)
     else:
         print("Fehler")
         return "Fehler"
 
 def nT(n, k):
-    out = binomial(n, k)
+    out = binomial(18, 8)
     return output(out, n, k)
 
 def output(out, n, k):
     print("\n"+n+" über "+k+" ist: \n"+str(out))
-    return "\nBinomialverteilung: "+'$$\binom{'+n+'}{'+k+'}'+str(latex(out))+"$$"
+    return "\nBinomialverteilung: "+str(latex(out))+"$$"
