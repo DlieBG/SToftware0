babb7bec86071ade1032866f8d1ee5b19d665036
boba2fett <boba2002fett@gmail.com>
2019-05-26 20:55:03 +0200

    next level

--- functions/chessproblem.py
+++ functions/chessproblem.py
@@ -11,6 +11,10 @@ def hook(keyinput):
             return queen()
         if "tower" in keyinput or "turm" in keyinput:
             return tower()
+        if "walker" in keyinput or "lauefer" in keyinput:
+            return walker()
+        if "jumper" in keyinput or "springer" in keyinput:
+            return jumper()
         return output(keyinput)
     return ""
     
@@ -171,6 +175,69 @@ def tower():
     E=linsolve((MT2,V),(symbols('a')))
     return "Matrix: $$"+latex(MT)+"$$ Stationaere Verteilung: $$"+latex(E)+"$$"
     
+def walker():
+    MW = Matrix([
+    [0,0,0,0,0,Fraction(1,5),0,0,0,0,Fraction(1,5),0,0,0,0,Fraction(1,3)],
+    [0,0,0,0,Fraction(1,3),0,Fraction(1,5),0,0,0,0,Fraction(1,3),0,0,0,0],
+    [0,0,0,0,0,Fraction(1,5),0,Fraction(1,3),Fraction(1,3),0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,Fraction(1,5),0,0,Fraction(1,5),0,0,Fraction(1,3),0,0,0],
+    [0,Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,5),0,0,0,0,Fraction(1,3),0],
+    [Fraction(1,3),0,Fraction(1,3),0,0,0,0,0,Fraction(1,3),0,Fraction(1,5),0,0,0,0,Fraction(1,3)],
+    [0,Fraction(1,3),0,Fraction(1,3),0,0,0,0,0,Fraction(1,5),0,Fraction(1,3),Fraction(1,3),0,0,0],
+    [0,0,Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,5),0,0,Fraction(1,3),0,0],
+    [0,0,Fraction(1,3),0,0,Fraction(1,5),0,0,0,0,0,0,0,Fraction(1,3),0,0],
+    [0,0,0,Fraction(1,3),Fraction(1,3),0,Fraction(1,5),0,0,0,0,0,Fraction(1,3),0,Fraction(1,3),0],
+    [Fraction(1,3),0,0,0,0,Fraction(1,5),0,Fraction(1,3),0,0,0,0,0,Fraction(1,3),0,Fraction(1,3)],
+    [0,Fraction(1,3),0,0,0,0,Fraction(1,5),0,0,0,0,0,0,0,Fraction(1,3),0],
+    [0,0,0,Fraction(1,3),0,0,Fraction(1,5),0,0,Fraction(1,5),0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,Fraction(1,3),Fraction(1,3),0,Fraction(1,5),0,0,0,0,0],
+    [0,0,0,0,Fraction(1,3),0,0,0,0,Fraction(1,5),0,Fraction(1,3),0,0,0,0],
+    [Fraction(1,3),0,0,0,0,Fraction(1,5),0,0,0,0,Fraction(1,5),0,0,0,0,0]
+    ])
+    return "Matrix: $$"+latex(MT)+"$$"
+    
+def jumper():
+    MJ2 = Matrix([
+    [0,0,0,0,0,0,Fraction(1,4),0,0,Fraction(1,4),0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,Fraction(1,3),Fraction(1,3),0,Fraction(1,4),0,0,0,0,0],
+    [0,0,0,0,Fraction(1,3),0,0,0,0,Fraction(1,4),0,Fraction(1,3),0,0,0,0],
+    [0,0,0,0,0,Fraction(1,4),0,0,0,0,Fraction(1,4),0,0,0,0,0],
+    [0,0,Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,4),0,0,Fraction(1,3),0,0],
+    [0,0,0,Fraction(1,2),0,0,0,0,0,0,0,Fraction(1,3),Fraction(1,2),0,Fraction(1,3),0],
+    [Fraction(1,2),0,0,0,0,0,0,0,Fraction(1,3),0,0,0,0,Fraction(1,3),0,Fraction(1,2)],
+    [0,Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,4),0,0,0,0,Fraction(1,3),0],
+    [0,Fraction(1,3),0,0,0,0,Fraction(1,4),0,0,0,0,0,0,0,Fraction(1,3),0],
+    [Fraction(1,2),0,Fraction(1,3),0,0,0,0,Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,2)],
+    [0,Fraction(1,3),0,Fraction(1,2),Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,2),0,0,0],
+    [0,0,Fraction(1,3),0,0,Fraction(1,4),0,0,0,0,0,0,0,Fraction(1,3),0,0],
+    [0,0,0,0,0,Fraction(1,4),0,0,0,0,Fraction(1,4),0,0,0,0,0],
+    [0,0,0,0,Fraction(1,3),0,Fraction(1,4),0,0,0,0,Fraction(1,3),0,0,0,0],
+    [0,0,0,0,0,Fraction(1,4),0,Fraction(1,3),Fraction(1,3),0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,Fraction(1,4),0,0,Fraction(1,4),0,0,0,0,0,0]
+    ])
+    
+    MJ2 = Matrix([
+    [0,0,0,0,0,0,Fraction(1,4),0,0,Fraction(1,4),0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,Fraction(1,3),Fraction(1,3),0,Fraction(1,4),0,0,0,0,0],
+    [0,0,0,0,Fraction(1,3),0,0,0,0,Fraction(1,4),0,Fraction(1,3),0,0,0,0],
+    [0,0,0,0,0,Fraction(1,4),0,0,0,0,Fraction(1,4),0,0,0,0,0],
+    [0,0,Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,4),0,0,Fraction(1,3),0,0],
+    [0,0,0,Fraction(1,2),0,0,0,0,0,0,0,Fraction(1,3),Fraction(1,2),0,Fraction(1,3),0],
+    [Fraction(1,2),0,0,0,0,0,0,0,Fraction(1,3),0,0,0,0,Fraction(1,3),0,Fraction(1,2)],
+    [0,Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,4),0,0,0,0,Fraction(1,3),0],
+    [0,Fraction(1,3),0,0,0,0,Fraction(1,4),0,0,0,0,0,0,0,Fraction(1,3),0],
+    [Fraction(1,2),0,Fraction(1,3),0,0,0,0,Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,2)],
+    [0,Fraction(1,3),0,Fraction(1,2),Fraction(1,3),0,0,0,0,0,0,0,Fraction(1,2),0,0,0],
+    [0,0,Fraction(1,3),0,0,Fraction(1,4),0,0,0,0,0,0,0,Fraction(1,3),0,0],
+    [0,0,0,0,0,Fraction(1,4),0,0,0,0,Fraction(1,4),0,0,0,0,0],
+    [0,0,0,0,Fraction(1,3),0,Fraction(1,4),0,0,0,0,Fraction(1,3),0,0,0,0],
+    [0,0,0,0,0,Fraction(1,4),0,Fraction(1,3),Fraction(1,3),0,0,0,0,0,0,0],
+    #[0,0,0,0,0,0,Fraction(1,4),0,0,Fraction(1,4),0,0,0,0,0,0]
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2]
+    ])
+    MJ2=MJ2-I
+    E=linsolve((MJ2,V),(symbols('a')))
+    return "Matrix: $$"+latex(MT)+"$$ Stationaere Verteilung: $$"+latex(E)+"$$"
     
     
 
