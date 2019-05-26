ed0d6e5e1c760f043b0e047abf72895fa07cd948
boba2fett <boba2002fett@gmail.com>
2019-05-26 21:00:00 +0200

    fix

--- functions/chessproblem.py
+++ functions/chessproblem.py
@@ -194,10 +194,10 @@ def walker():
     [0,0,0,0,Fraction(1,3),0,0,0,0,Fraction(1,5),0,Fraction(1,3),0,0,0,0],
     [Fraction(1,3),0,0,0,0,Fraction(1,5),0,0,0,0,Fraction(1,5),0,0,0,0,0]
     ])
-    return "Matrix: $$"+latex(MT)+"$$"
+    return "Matrix: $$"+latex(MW)+"$$"
     
 def jumper():
-    MJ2 = Matrix([
+    MJ = Matrix([
     [0,0,0,0,0,0,Fraction(1,4),0,0,Fraction(1,4),0,0,0,0,0,0],
     [0,0,0,0,0,0,0,Fraction(1,3),Fraction(1,3),0,Fraction(1,4),0,0,0,0,0],
     [0,0,0,0,Fraction(1,3),0,0,0,0,Fraction(1,4),0,Fraction(1,3),0,0,0,0],
@@ -237,7 +237,7 @@ def jumper():
     ])
     MJ2=MJ2-I
     E=linsolve((MJ2,V),(symbols('a')))
-    return "Matrix: $$"+latex(MT)+"$$ Stationaere Verteilung: $$"+latex(E)+"$$"
+    return "Matrix: $$"+latex(MJ)+"$$ Stationaere Verteilung: $$"+latex(E)+"$$"
     
     
 
