722c0fa9b07df9abbe887a765c31fa16f0aa08a1
Benedikt <mail@benedikt-schwering.de>
2019-10-09 17:18:16 +0200

    Update fcm.py

--- functions/fcm.py
+++ functions/fcm.py
@@ -20,7 +20,7 @@ def send(keyinput):
     push_service = FCMNotification(api_key="AAAAOL_f7Vw:APA91bHx4r3FM3rH2RxU2JtjoMRMm8At-c9XGrUnxpp6BCHtQLup1uvjAzIifOBAMMqCDOi_Z0O6wgqYGMtVJdt4EUdVzBXiUmpP_kTJRWB_9TmJBTp2II3tELBvSBhQvxMZxxLYP_Xu")
 
     registration_ids = [
-        "czKru1lRZX4:APA91bE3VVc_iwDxTaYVtHljlA1OQebkZHyCGhKjwb-n9AeNwXGwMQOd71kHmwK20hFB5fDQCSfSRc-piirHH-swvYSrcURiMmLLXMq3EC3TSGW6cSstqPqpeNVKSi3hwOsQwGmN1SsM",
+        "dSvcsDop4ek:APA91bFcfxJak8NLbyxjpXavu3RLbf28jNLRRcAcTTN7_dzg-MS8rzN3uRsPvMgpmPNvpJCaRIFMOliUDX169uf3cH2V7isYYMa0F6Hn5VZRunVhz_ZJBUot5m9V2HotGtbBxtAULUvM",
         #"fg09UzZglnc:APA91bHThBBv5LA-VmM3xrU1NkfCMqyZ5cR9_50PoiG_MNXpmJdoQ2XhX2s9hiFFQZnHirwLq032YH0HaTy_EBBX7YfKwiUr1disox8had4zU9Lo1DO9KP1PiExiYP9cMCd6LSNLdmP9"
                         ]
     message_title = "SToftware0 Call"
