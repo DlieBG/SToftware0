8000adb9902194aa349aeaa596b7c19dfc80db7a
boba2fett <boba2002fett@gmail.com>
2020-06-10 14:05:40 +0200

    new branch Yeet

--- expr.py
+++ expr.py
@@ -17,13 +17,3 @@ def findTerm(keyinput):
             return part
 
     return ""
-
-def isKeyword(s):
-    keywords=["ableit","derivate"]#TODO add all options (if included)
-    for phrase in keywords:
-        if phrase in s:
-            return True
-    return False
-
-#good improvement
-##a commit a day keeps the doctor away
